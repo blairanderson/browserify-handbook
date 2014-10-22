6c36641e39a735bfa2af777173438dbf87500575
Blair Anderson <Blair@quickleft.com>
2014-10-22 09:05:48 -0700

    add module2

---
+++ module2.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+url = 'http://api.jquery.com/';
+
+request(url, function (error, response, html) {
+  var results = [];
+
+  if (!error) {
+    var $ = cheerio.load(html);
+    $('article.post').each(function () {
+      var data = $(this);
+      results.push({
+        title: data.children().first().text(),
+        decription: data.children().first().text(),
+        href: data.children().first().text()
+      });
+    });
+  }
+
+  fs.writeFile('output.json', JSON.stringify(results, null, 4), function (err) {
+    if (err){
+      console.log(err);
+    }
+    console.log('File success');
+  });
+
+});
